ec05ae0871c8f38d9e6f0a238dcc82260c66ef3a
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2020-04-13 23:41:30 +0530

    Update command line arguments.cpp

--- case-1/command line arguments.cpp
+++ case-1/command line arguments.cpp
@@ -1,8 +1,3 @@
-/*purpose	:parse the command line and dispaly the output
-  filename	:command line arguments.cpp
-  Author 	:Badari vinay
-  Date   	:08/04/2020
-*/
 #include<iostream>
 #include<string.h>
 #include<stdlib.h>
@@ -14,19 +9,20 @@ bool checkDatatype(string sStr);
 //main using command line arguments
 int main(int argc,char *argv[])
 {
-	if((argc<=2) || (strcmp(argv[1],"-h")==0))
+	//if((argc==2) || (strcmp(argv[1],"-h")==0))
+	if(argc==2)
 		{
 			//if loop for comparing the input string with "-h" and displaying the usage	              
 	    	{
-	    		cout<<"              Usage Of The File           "<<endl;
-				cout<<argv[0]<<"datatype1 datatype2 .... datatypeN "<<endl; 
+	    		cout<<" Usage Of The File Name "<<endl;
+				cout<<argv[0]<<endl<<"datatype1 datatype2 .... datatypeN "<<endl; 
 	    	}
-	}
+	    }
     else
     {
 	    int iJ=2;
-	    cout<<"_______________________________________________________"<<endl;
-		cout<<"	 type	"<<"\t"<<"value	"<<"\t"<<"size	"<<endl;
+	    //cout<<argv[0]<<endl;
+		cout<<"	 Type  "<<"\t"<<"    Value    "<<"\t"<<"    Size    "<<endl;
 	    //while loop for checking the argcount
 		while(iJ<argc)                                      
 	    {
@@ -35,11 +31,16 @@ int main(int argc,char *argv[])
 		    if(iA==0)									
 		    {
 			    if(strlen(argv[iJ])==1)
+			    {
 				    cout<<endl<<"\t char";
+				    cout<<"\t\t"<<argv[iJ]<<"\t\t"<<strlen(argv[iJ]);
+			    }
 				else
+				{
 				    cout<<endl<<"\t string";
-				    cout<<"\t\t"<< argv[iJ]<<"\t\t"<<strlen(argv[iJ]);
-		    }
+				    cout<<"\t\t"<< argv[iJ]<<"\t\t"<<sizeof(argv[iJ]);
+		        }
+	        }
 		    //else case for displaying int and float values
 			else									
 		    {
