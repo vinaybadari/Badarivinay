204e3fc74882934ca079ec6e2e8620ba52cf95cc
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2020-04-21 12:01:57 +0530

    Update access specifiers.cpp

--- case-4/access specifiers.cpp
+++ case-4/access specifiers.cpp
@@ -10,54 +10,88 @@ using namespace std;
 class base
 {
  	private:
-        int Ij;
+        int Iprivatedata1;
+        int Iprivatedata2;
  	protected:
- 	    int Ik;
+ 	    int Iprotecteddata1;
+ 	    int Iprotecteddata2;
  	public:
- 	    int Il;
+ 	    int Ipublicdata1;
+ 	    int Ipublicdata2;
 	// Default constructor
  	base() 
  	{
- 	   Ij = 1;
- 	   Ik = 2;
- 	   Il = 3;
+ 	   Iprivatedata1 = 1;
+ 	   Iprotecteddata1 = 2;
+ 	   Ipublicdata1 = 3;
  	}
+ 	public:
+	
+		void show()
+		{
+			cout<<"\nEnter any 3 values for base class: "<<endl;
+				cin>>Iprivatedata2;
+				cin>>Iprotecteddata2;
+				cin>>Ipublicdata2;
+			//Every members can be access here, same class
+			cout<<"\nAccessing variable within the class"<<endl;
+			
+			cout<<"Value of Iprivatedata2		: "<<Iprivatedata2<<endl;
+			cout<<"Value of Iprotecteddata2		: "<<Iprotecteddata2<<endl;
+			cout<<"Value of Ipublicdata2		: "<<Ipublicdata2<<endl;
+		}
+	
 };
 class derive: private base
 {
-	//Ik and Il becomes private members of class derive and x remains private
  	public:
  	    void showdata()
  	    {
- 	       cout <<  "\t   *** Using Accessmode as private***    " << endl;
-               cout <<  "\t   j is not Accessible because we declared as private " << endl;             
-               cout <<  "\t   value of k is   " << Ik << endl;
-               cout <<  "\t   value of l is   " << Il << endl;
+ 	       	   cout <<  "\t   *** Using Accessmode as private***    " << endl;
+               cout <<  "\t   privatedata1 is not Accessible because we declared as private " << endl;             
+               cout <<  "\t   value of protecteddata1 is   	" << Iprotecteddata1 << endl;
+               cout <<  "\t   value of publicdata1 is   	" << Ipublicdata1 << endl;
  	    }
 };
-class derivea: protected base
+class derivea: public base
 {
-	//Ik and Il becomes protected members of class derive
  	public:
  	    void showdata()
  	    {
- 	       cout<<   "\t   *** Using Accessmode as protected***  " << endl;
- 	       cout <<  "\t   j is not Accessible because we declared as private " << endl;
-               cout <<  "\t   value of k is   " << Ik << endl;
-               cout <<  "\t   value of l is    " << Il << endl;
+ 	           cout<<   "\t   *** Using Accessmode as protected***  " << endl;
+ 	           cout <<  "\t   privatedata1 is not Accessible because we declared as private " << endl;
+               cout <<  "\t   value of protecteddata1 is   	" << Iprotecteddata1 << endl;
+               cout <<  "\t   value of publicdata1 is    	" << Ipublicdata1 << endl;
  	    }
 };
-class deriveb: public base
+class deriveb: protected base
 {
-	 	//Ik becomes protected and Il becomes public members of class derive
  	public:
  	    void showdata()
  	    {
- 	       cout<<   "\t   *** Using Accessmode as public*** " << endl;;
- 	       cout <<  "\t   j is not Accessible because we declared as private " << endl;
-               cout <<  "\t   value of k is   " << Ik << endl;
-               cout <<  "\t   value of l is   " << Il << endl;
+ 	       	   cout<<   "\t   *** Using Accessmode as public*** " << endl;;
+ 	           cout <<  "\t   privatedata1 is not Accessible because we declared as private " << endl;
+               cout <<  "\t   value of protecteddata1 is   	" << Iprotecteddata1 << endl;
+               cout <<  "\t   value of publicdata1 is   	" << Ipublicdata1 << endl;
  	    }
+
+};
+class derivec:	public base
+{
+		public:
+		void show()
+		{
+			cout<<"\nEnter any 2 values for sub_class: "<<endl;
+				cin>>Iprotecteddata2;
+				cin>>Ipublicdata2;
+			cout<<"\nAccessing variable in Sub class"<<endl;
+		
+			// privatedata2 is not accessible here it is private
+			//publicdata2 is public so it is accessible any where
+			cout<<"Value of protecteddata2:	 	"<<Iprotecteddata2<<endl;
+			//'protecteddata2' is declared as protected, so it is accessible in sub class
+			cout<<"Value of publicdata2: 		"<<Ipublicdata2<<endl;
+		}
 };
 int main(int argc,char *argv[])
 {
@@ -73,12 +107,25 @@ int main(int argc,char *argv[])
 	}
 	else
 	{
+	 // create object
+	 base d; 
+	 d.show();
+	 // create object
+	 derivec s; 
+	 s.show();    
+	 cout<<"\nAccessing variable Outside the class"<<endl;
+	//'privatedata2' cannot be accessed as it is private
+	//'publivdata2' is public as can be accessed from any where
+	//can also access derivec b variable  's.publicdata2'
+	cout<<"value of publicdata2: "<<d.Ipublicdata2<<endl; 
+	//'protecteddata2' is protected and cannot be accesed here
+	//cout<<"value of c: "<<d.b<<endl;
      derive a;
      derivea b;
      deriveb c;
      a.showdata();
      b.showdata();
      c.showdata();
-     return 0;
+
 	} 
 }
