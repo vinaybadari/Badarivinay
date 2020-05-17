a2c5c4b881bf07e9a64fcc0078b7b04f5e8f0429
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2020-05-18 00:04:31 +0530

    Create Lettercombinationmain.cpp

---
+++ case-2/Lettercombinationmain.cpp
@@ -0,0 +1,32 @@
+#include "Lettercombination.h"
+#include <iostream>
+#include<string.h>
+
+using namespace std;
+int main(int argc,char* argv[])
+{
+	Lettercombination Obj;
+    if(argc==2 && strcmp(argv[1],"-h")==0)
+    {
+        //created help command
+        cout<<"Enter Input"<<endl;
+    }
+    else
+    {
+   vector<string> keyboard = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+   vector<int> digits = Obj.getNumber();
+   vector<string> wordlist(1,"");
+   vector<string> temp;
+
+   for ( int i : digits )
+   {
+      for ( string s : wordlist )
+      {
+         for ( char c : keyboard[i] ) temp.push_back( s + c );
+      }
+      wordlist = temp;
+      temp.clear();
+   }
+   for ( string s : wordlist ) cout << s << endl;
+}
+}
