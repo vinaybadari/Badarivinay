68a0a3042e5459496f9a0bbffbc54b936567f081
vinaybadari <40800889+vinaybadari@users.noreply.github.com>
2020-05-18 00:05:53 +0530

    Create Lettercombination.h

---
+++ case-2/Lettercombination.h
@@ -0,0 +1,14 @@
+#ifndef Lettercombination_h
+#define Lettercombination_h
+
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Lettercombination
+{
+	public:
+   vector<int> getNumber();
+   //vector<int> digits();
+};
+#endif
